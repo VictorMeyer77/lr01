eb2025594e7ac0a399aef3cbbbea5bf31ccb3209
Victor-Algo <victor.algo.contact@gmail.com>
2023-04-24 00:12:28 +0200

    change pygame to vlc for play audio

--- lr01/sense/mouth.py
+++ lr01/sense/mouth.py
@@ -1,6 +1,6 @@
 import gtts
-import pygame
 import os
+import vlc
 
 OUTPUT_AUDIO_BUFFER_PATH = "resources/output/audio/mouth/buffer.wav"
 
@@ -10,17 +10,18 @@ class Mouth:
     def __init__(self, buffer_path=OUTPUT_AUDIO_BUFFER_PATH, language="fr"):
         self.buffer_path = buffer_path
         self.language = language
-        pygame.init()
+        self.player = vlc.MediaPlayer()
 
     def _convert_text_to_sound(self, text):
         tts = gtts.gTTS(text, lang=self.language)
         tts.save(self.buffer_path)
 
     def _play_sound(self):
-        audio = pygame.mixer.Sound(self.buffer_path)
-        channel = audio.play()
-        while channel.get_busy():
-            pygame.time.wait(100)
+        media = vlc.Media(self.buffer_path)
+        self.player.set_media(media)
+        self.player.play()
+        while self.player.get_state() not in [4, 5, 6, 7]:
+            pass
 
     def run(self, text):
         self._convert_text_to_sound(text)
