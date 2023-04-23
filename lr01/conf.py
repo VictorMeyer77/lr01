04d44ceac1a96b7fdbfa963072ff4aaff9cabcdc
Victor-Algo <victor.algo.contact@gmail.com>
2023-04-23 20:48:34 +0200

    add conf class

---
+++ lr01/conf.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Conf:
+
+    def __init__(self, conf_dir_path):
+        self.conf_dir_path = conf_dir_path
+
+    def get_chat_gpt_key(self):
+        with open(os.path.join(self.conf_dir_path, "chatgpt.key"), "r") as file:
+            return file.readlines()[0]
