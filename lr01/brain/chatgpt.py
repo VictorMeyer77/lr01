bbeb9d29d99772983633c71b8f4e8520b1d40cb9
Victor-Algo <victor.algo.contact@gmail.com>
2023-04-23 23:42:36 +0200

    simplify conf

--- lr01/brain/chatgpt.py
+++ lr01/brain/chatgpt.py
@@ -1,11 +1,9 @@
 import openai
-from lr01.conf import Conf
 
 
 class ChatGPT:
 
-    def __init__(self, conf_path):
-        conf = Conf(conf_path)
+    def __init__(self, conf):
         openai.api_key = conf.get_chat_gpt_key()
 
     @staticmethod
